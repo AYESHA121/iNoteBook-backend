7306787f3582b6dfd2ccd51ae1ed82abcb4e242c
AYESHA121 <62888478+AYESHA121@users.noreply.github.com>
2023-01-01 20:34:27 -0800

    cor disable

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const connectToMongo = require("./db");
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const serverless = require("serverless-http");
 
 connectToMongo();
@@ -9,7 +9,7 @@ const app = express();
 
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 
 
