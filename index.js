c7bb90ade52f37c73b57ce59ce0a5da5a7cb3fe1
AYESHA121 <62888478+AYESHA121@users.noreply.github.com>
2023-01-01 20:12:29 -0800

    netlifychanged

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const connectToMongo = require("./db");
 const express = require("express");
 const cors = require("cors");
+const serverless = require("serverless-http");
 
 connectToMongo();
 const app = express();
-const port = 5000;
+// const port = process.env.PORT || 5000;
+
 
 app.use(express.json());
 app.use(cors());
 
-// Available Routes
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/notes", require("./routes/notes"));
 
-app.listen(port, () => {
-    console.log(`iNotebook listening at http://localhost:${port}`);
-});
+
+// Available Routes
+app.use("/.netlify/functions/api/auth", require("./routes/auth"));
+app.use("/.netlify/functions/api/notes", require("./routes/notes"));
+
+// app.listen(port, () => {
+//     console.log(`iNotebook listening at https://inotebook-cloud-api.onrender.com:${port}`);
+// });
+
+
+module.exports = app;
+module.exports.handler = serverless(app);
